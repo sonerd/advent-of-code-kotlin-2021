7642538892ebfd37678deb5ae5d47f710390053d
Soner Dastan <s.dastan@bilduin.de>
2021-12-03 22:07:11 +0100

    minor optimizations

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,59 +1,57 @@
 fun main() {
     val input = readInput("Day02")
 
-    val forward = "forward"
-    val down = "down"
-    val up = "up"
-
-    val result = calculateDivePart1(input, forward, down, up)
+    val result = calculateDivePart1(input)
     println("""part one: depth x horizontal =  $result""")
 
     val testInputPart2 = readInput("Day02_part2")
-    val testResultPart2 = calculateDivePart2(testInputPart2, forward, down, up)
+    val testResultPart2 = calculateDivePart2(testInputPart2)
     println("""test for part two: depth x horizontal =  $testResultPart2""")
     check(testResultPart2 == 900)
 
     val inputPart2 = readInput("Day02")
-    val resultPart2 = calculateDivePart2(inputPart2, forward, down, up)
+    val resultPart2 = calculateDivePart2(inputPart2)
     println("""part two: depth x horizontal =  $resultPart2""")
 }
 
-fun calculateDivePart1(testInput: List<String>, forward: String, down: String, up: String): Int {
+fun calculateDivePart1(testInput: List<String>): Int {
     var depth = 0
     var horizontal = 0
     testInput.forEach {
-        when {
-            it.contains(forward) -> {
-                val forwardValue = Integer.valueOf(it.substringAfterLast(forward).trim())
+        val line = it.split(" ")
+        when (line[0]) {
+            "forward" -> {
+                val forwardValue = Integer.valueOf(line[1])
                 horizontal += forwardValue
             }
-            it.contains(down) -> {
-                depth += Integer.valueOf(it.substringAfterLast(down).trim())
+            "down" -> {
+                depth += Integer.valueOf(line[1])
             }
-            it.contains(up) -> {
-                depth -= Integer.valueOf(it.substringAfterLast(up).trim())
+            "up" -> {
+                depth -= Integer.valueOf(line[1])
             }
         }
     }
     return depth * horizontal
 }
 
-private fun calculateDivePart2(testInput: List<String>, forward: String, down: String, up: String): Int {
+private fun calculateDivePart2(testInput: List<String>): Int {
     var depth = 0
     var horizontal = 0
     var aim = 0
     testInput.forEach {
-        when {
-            it.contains(forward) -> {
-                val forwardValue = Integer.valueOf(it.substringAfterLast(forward).trim())
+        val line = it.split(" ")
+        when (line[0]) {
+            "forward" -> {
+                val forwardValue = Integer.valueOf(line[1])
                 horizontal += forwardValue
                 depth += aim * forwardValue
             }
-            it.contains(down) -> {
-                aim += Integer.valueOf(it.substringAfterLast(down).trim())
+            "down" -> {
+                aim += Integer.valueOf(line[1])
             }
-            it.contains(up) -> {
-                aim -= Integer.valueOf(it.substringAfterLast(up).trim())
+            "up" -> {
+                aim -= Integer.valueOf(line[1])
             }
         }
     }
