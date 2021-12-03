fb7223720d657c9d86600431bea2bbded079df78
Soner Dastan <s.dastan@bilduin.de>
2021-12-03 22:26:47 +0100

    minor optimizations

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,34 +1,28 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increasedCount = 0
+        input.forEachIndexed { i, e ->
+            val valueAsInt = Integer.valueOf(e)
+            if (i > 0 && valueAsInt > Integer.valueOf(input[i - 1])) {
+                increasedCount++
+            }
+        }
+        return increasedCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    val testInput = readInput("Day01")
-    check(part1(testInput) == 2000)
-    println("""increased measurements: ${countIncreasedMeasurements(testInput)}""")
-
-    val listOfSums = mutableListOf<String>()
-    testInput.forEachIndexed { i, e ->
-        val currentValue = Integer.valueOf(e)
-        if (i + 2 < testInput.size) {
-            val sum = currentValue + Integer.valueOf(testInput[i + 1]) + Integer.valueOf(testInput[i + 2])
-            listOfSums.add(sum.toString())
+        val listOfSums = mutableListOf<String>()
+        input.forEachIndexed { i, e ->
+            val currentValue = Integer.valueOf(e)
+            if (i + 2 < input.size) {
+                val sum = currentValue + Integer.valueOf(input[i + 1]) + Integer.valueOf(input[i + 2])
+                listOfSums.add(sum.toString())
+            }
         }
+        return part1(listOfSums)
     }
-    println("""increased sum of measurements: ${countIncreasedMeasurements(listOfSums)}""")
-}
 
-private fun countIncreasedMeasurements(testInput: List<String>): Int {
-    var increasedCount = 0
-    testInput.forEachIndexed { i, e ->
-        val valueAsInt = Integer.valueOf(e)
-        if (i > 0 && valueAsInt > Integer.valueOf(testInput[i - 1])) {
-            increasedCount++
-        }
-    }
-    return increasedCount
+    val input = readInput("Day01")
+    println("""increased measurements: ${part1(input)}""")
+    println("""increased sum of measurements: ${part2(input)}""")
 }
